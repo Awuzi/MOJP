8c7f40cd54fd4a07802bce20ce3a6c99eceda116
Lamri Yahia <yahia.lamri@outlook.fr>
2019-04-17 10:06:58 +0200

    :)

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ if (isset($_POST['editNote'])) UpdateNote($_POST['editID'], $_POST['note']);
                             $email = $selectCustomer->email;
 
                             if (endsWith($email, "marketplace.amazon.co.uk")) $email = "AZ"; ?>
-                            <tr class="<?php echo $themeMode; ?>">
+                            <tr>
                                 <td><?php echo $idOrder; ?></td>
                                 <td><?php echo $email . "\n"; ?></td>
                                 <td><?php echo $selectCustomer->firstname . " " . $selectCustomer->lastname . "\n"; ?></td>
@@ -53,12 +53,6 @@ if (isset($_POST['editNote'])) UpdateNote($_POST['editID'], $_POST['note']);
                                     }
                                     ?>
                                 </td>
-                                <style>
-
-                                    #act {
-                                        background-color: ;
-                                    }
-                                </style>
                                 <td>
                                     <a href="?OrderId=<?php echo $element->id_order; ?>">
                                         <button type="button" class="btn <?php echo $outline;?>">
